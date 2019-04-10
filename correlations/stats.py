ed5182b093fafcdd8b6a5a701011c2d8595080f3
braudelan <braudelan@gmail.com>
2019-04-10 12:19:13 +0300

    control_graphs.py + stats_table.py adedd

--- correlations/stats.py
+++ correlations/stats.py
@@ -17,4 +17,4 @@ for test, number in zip(TESTS, NUMBERS):
     raw_data.columns.set_levels(["c", "t"], level=1, inplace=True)
 
     #get statistics and parameters
-    means, means_stde, treatment_effect = get_stats(raw_data)
+    means, means_stde, effect = get_stats(raw_data)
